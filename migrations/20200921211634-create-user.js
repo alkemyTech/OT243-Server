30a708ca52cdb8820f3fd3f5dc7d35581e66a891
Santiago Reynoso <snreynoso@gmail.com>
2022-07-15 17:00:37 -0300

    Model user atributes updated

--- migrations/20200921211634-create-user.js
+++ migrations/20200921211634-create-user.js
@@ -9,19 +9,24 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       roleId: {
         type: Sequelize.INTEGER,
