7419a0f8900671b5745bccda791917070be8d3e5
Eric Casanova <kilbane1407@gmail.com>
2022-07-15 12:44:32 -0300

    controller added

---
+++ services/category.js
@@ -0,0 +1,10 @@
+
+
+class CategoryService{
+    //TODO:implementar con el modelo adecuado
+    static async findAll(){
+        return ["cat1","cat2"]
+    }
+}
+
+module.exports = CategoryService;
