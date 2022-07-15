9567dfc7b1b29fb4d66084b77d27bd50294722db
Eric Casanova <kilbane1407@gmail.com>
2022-07-15 18:50:42 -0300

    category base controller added}

--- controllers/category.js
+++ controllers/category.js
@@ -1,11 +1,16 @@
 const {CLIENT_ERROR} = require("../utils/httpCodes.js");
 const CategoryService = require("../services/category.js");
 
-export const getCategories = async (req,res) => {
+const getCategories = async (req,res) => {
     try {
         const categories = await CategoryService.findAll();
         res.json(categories);
     } catch (error) {
         res.status(CLIENT_ERROR).json(error);
     }
+}
+
+
+module.exports= {
+    getCategories
 }
