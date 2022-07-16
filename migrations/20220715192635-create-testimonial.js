18190ee1455c7e85d3052c3de5fc9ae7cca11105
Santiago Reynoso <snreynoso@gmail.com>
2022-07-15 21:51:13 -0300

    Testimonials migration typing error

--- migrations/20220715192635-create-testimonial.js
+++ migrations/20220715192635-create-testimonial.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       content: {
         type: Sequelize.STRING,
-        allowNull: true,
+        allowNull: true
       },
       createdAt: {
         allowNull: false,
