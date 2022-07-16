99e52bc9f7087c8e3dfb1033479150c5d317ac7c
Santiago Reynoso <snreynoso@gmail.com>
2022-07-15 21:21:39 -0300

    Testimonial new seed

--- seeders/20220716001921-create-testimonial.js.js
+++ seeders/20220716001921-create-testimonial.js.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Testimonials', [
       {
         name: 'John Doe',
@@ -19,7 +19,7 @@ module.exports = {
       }], {});
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
