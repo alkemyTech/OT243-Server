cf51120ec89c45549825bca8668ec4ad3033d511
mfernandez <mfernandez@ultimamillasa.com.ar>
2022-07-22 15:37:56 -0300

    changed name function from testEmail to sendEmail

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -1,7 +1,7 @@
 const sgMail = require('@sendgrid/mail');
 
 module.exports = {
-  testEmail: function (toEmail, subject, title, message) {
+  sendEmail: function (toEmail, subject, title, message) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: toEmail,
