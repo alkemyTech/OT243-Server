6be9a7da6eb3273967af6167ca67180f3a597c10
Eric Casanova <kilbane1407@gmail.com>
2022-07-28 15:16:39 -0300

    modify category added

--- services/category.js
+++ services/category.js
@@ -3,10 +3,13 @@ const { Category } = require("../models");
 class CategoryService{
     //TODO:implementar con el modelo adecuado
     static async findAll(){
-        return await Category.findAll({attributes: ["name"]});
+        return await Category.findAll({ attributes: ["name"] });
     }
     static async findByPk(id){
         return await Category.findByPk(id)
+    }
+    static async updateCategory(id, content){
+        return await Category.update({ ...content }, { where: { id } })
     }
 }
 
