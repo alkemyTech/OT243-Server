e5045896144e1377c4ccf80d1465898c924d09cf
Cristian Unigarro <ungcrisfit@gmail.com>
2022-08-06 10:34:08 -0500

    add: get route

--- routes/news.js
+++ routes/news.js
@@ -5,7 +5,10 @@ const { validateCreate } = require('../validators/newsValidator');
 const router = express.Router();
 
 // TODO: Implement Protected Route for admin
-router.route('/').post(validateCreate, newsController.createNews);
+router
+  .route('/')
+  .get(newsController.getAllNews)
+  .post(validateCreate, newsController.createNews);
 router.route('/:id').get(newsController.getNews).put(newsController.updateNews);
 
 module.exports = router;
