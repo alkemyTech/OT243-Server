e6e3a9f6487f4387cbe7e428a09e2c014e8747d4
Santiago Reynoso <snreynoso@gmail.com>
2022-08-15 10:13:57 -0300

    Se creó el modelo Contact y se corrigieron errores de route user

--- services/contact.js
+++ services/contact.js
@@ -1,14 +1,8 @@
-const ContactModel = require("../models").Contact;
-/* TICKET 55
-Al completar el Formulario de Contacto, se almacenará en la base de datos. Nombre de tabla: contacts. 
-Contendrá los campos name, phone, email, message, deletedAt (utilizada para soft delete)
-
-*/
-
+const { Contact } = require('../models/index');
 
 class ContactService {
   static async getAllBackOfficeContact() {
-    const allContact = await ContactModel.findAll({
+    const allContact = await Contact.findAll({
     attributes: ["name", "phone", "email", "message", "createdAt"],
   });
     if (allContact) {
