2650a98bf96b54a101098028dd45618b089ecfea
Cristian Unigarro <ungcrisfit@gmail.com>
2022-08-16 08:16:26 -0500

    add: members route

--- routes/member.js
+++ routes/member.js
@@ -1,9 +1,9 @@
-const  express = require('express');
-const  router = express.Router();
+const express = require('express');
+const memberController = require('../controllers/membersController');
+const { validateMember } = require('../validators/memberValidator');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource Generic');
-});
+const router = express.Router();
+
+router.route('/').post(validateMember, memberController.createMember);
 
 module.exports = router;
